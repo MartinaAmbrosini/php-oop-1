55eff84c354592333413850f7741cb88cc1b2ca4
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-02-02 16:07:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+class Movie
+{
+    public $title;
+    public $genre;
+    public $score;
+
+    function __construct($title, $genre, $score)
+    {
+
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->score = $score;
+    }
+}
+
+$title1 = new Movie("La Città Incantata", "Animazione", "4,8");
+$title2 = new Movie("Il Castello Errante di Howl", "Animazione", "4,9");
+
+var_dump($title1);
+echo "<br><br>";
+
+var_dump($title2);
+echo "<br><br>";
